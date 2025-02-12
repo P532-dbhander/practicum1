cccf63622e9a34d3e5620c3ebd1afcfbb3c33d3b
dbhander <dbhander@iu.edu>
2025-02-12 14:42:32 -0500

    first-commit

---
+++ src/main/java/com/example/practicum1/Practicum1Application.java
@@ -0,0 +1,13 @@
+package com.example.practicum1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practicum1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practicum1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practicum1/Practicum1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicum1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practicum1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
